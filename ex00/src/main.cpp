8bed1003b708a4faa485ce976de0b6e82fecd97a
Emmatosorus <emma@bouteyres.com>
2024-09-07 14:19:34 +0200

    Add ✨: Finished ex00 with test

---
+++ ex00/src/main.cpp
@@ -0,0 +1,26 @@
+#include "../includes/whatever.h"
+#include <iostream>
+
+int main(void)
+{
+	std::cout << std::endl << "\x1b[1;36m############### TEST 1 ###############\x1b[0m" << std::endl << std::endl;
+	try
+	{
+		int a = 2;
+		int b = 3;
+		::swap( a, b );
+		std::cout << "a = " << a << ", b = " << b << std::endl;
+		std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
+		std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
+		std::string c = "chaine1";
+		std::string d = "chaine2";
+		::swap(c, d);
+		std::cout << "c = " << c << ", d = " << d << std::endl;
+		std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
+		std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
+	}
+	catch(std::exception & e)
+	{
+		std::cout << e.what() << std::endl;
+	}
+}
