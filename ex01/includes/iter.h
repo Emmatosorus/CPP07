3ee97908d867191ea2e1035389bab49e0eaeffe8
Emmatosorus <emma@bouteyres.com>
2024-09-07 14:44:30 +0200

    Add ✨: Finished ex01 with tests

---
+++ ex01/includes/iter.h
@@ -0,0 +1,13 @@
+#ifndef WHATEVER_H
+#define WHATEVER_H
+
+#include <iostream>
+
+template <typename T>
+void iter(T *t1, size_t t2, void func(T&))
+{
+	for (size_t i = 0; i < t2; i++)
+		func(t1[i]);
+}
+
+#endif
