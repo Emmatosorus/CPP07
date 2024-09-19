8a6441cdd66380ee8834ee78e07447eb69300f29
Emma Politzer <epolitze@z3r6p3.42lyon.fr>
2024-09-19 10:54:08 +0200

    Add ✨: Added tests and moved template function from .cpp to .h

--- ex01/includes/iter.h
+++ ex01/includes/iter.h
@@ -4,6 +4,12 @@
 #include <iostream>
 
 template <typename T>
+void	print(T &t)
+{
+	std::cout << t << std::endl;
+}
+
+template <typename T>
 void iter(T *t1, size_t t2, void func(T&))
 {
 	for (size_t i = 0; i < t2; i++)
