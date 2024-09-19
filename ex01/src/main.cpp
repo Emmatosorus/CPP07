01acad161079153d70209e960adfc1ee1a20e7e0
Emma Politzer <epolitze@z3r6p3.42lyon.fr>
2024-09-19 10:54:08 +0200

    Add ✨: Added tests and moved template function from .cpp to .h

--- ex01/src/main.cpp
+++ ex01/src/main.cpp
@@ -1,12 +1,6 @@
 #include "../includes/iter.h"
 #include <iostream>
 
-template <typename T>
-void	print(T &t)
-{
-	std::cout << t << std::endl;
-}
-
 int main(void)
 {
 	std::cout << std::endl << "\x1b[1;36m############### TEST 1 ###############\x1b[0m" << std::endl << std::endl;
@@ -23,6 +17,28 @@ int main(void)
 	try
 	{
 		int nb[3] = {0, 21, 42};
+		::iter(nb, 3, print);
+		std::cout << std::endl;
+	}
+	catch(std::exception & e)
+	{
+		std::cout << e.what() << std::endl;
+	}
+	std::cout << std::endl << "\x1b[1;36m############### TEST 3 ###############\x1b[0m" << std::endl << std::endl;
+	try
+	{
+		const int nb[3] = {0, 21, 42};
+		::iter(nb, 3, print);
+		std::cout << std::endl;
+	}
+	catch(std::exception & e)
+	{
+		std::cout << e.what() << std::endl;
+	}
+	std::cout << std::endl << "\x1b[1;36m############### TEST 4 ###############\x1b[0m" << std::endl << std::endl;
+	try
+	{
+		const char nb[3] = {'a', 'b', 'c'};
 		::iter(nb, 3, print);
 		std::cout << std::endl;
 	}
